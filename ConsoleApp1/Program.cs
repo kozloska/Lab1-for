6b2faf4be6cd95548733831c87fec0f77d73aaa9
stud <stud@I21013401494>
2022-11-02 12:52:20 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace FirstApp
+{
+    public class Logic
+    {
+        public static string Dupl(int[] nums, int size)
+        {
+            string outmassage = "";
+            for (int i = 1; i < size; i++)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    outmassage += "Индексы пары: ";
+                    outmassage += Convert.ToString(i - 1);
+                    outmassage = outmassage + " " + "и" + " ";
+                    outmassage += Convert.ToString(i);
+                    break;
+                }
+            }
+            return outmassage;
+        }
+        public static string Mas(int[] nums, int size)
+        {
+            string outmassage = Logic.Dupl(nums, size);
+            if (outmassage.Length == 0)
+            {
+                outmassage = "Нет такой пары чисел в последовательности";
+            }
+            return outmassage;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество чисел в последовательности: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите последовательность: ");
+            int[] nums = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            string outmassage = Logic.Mas(nums, size);
+            Console.Write(outmassage);
+        }
+    }
+}
---
+++ ConsoleApp1Tests/LogicTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void GetTest1()
+        {
+            int[] nums = { 1, 3, 3, 6, 7 };
+            string massage = Logic.Mas(nums, 5);
+            Assert.AreEqual("Индексы пары: 1 и 2", massage);
+        }
+        [TestMethod()]
+        public void Getest2()
+        {
+            int[] nums = { 1, 2, 3, 3, 3 };
+            string massage = Logic.Mas(nums, 5);
+            Assert.AreEqual("Индексы пары: 2 и 3", massage);
+        }
+        [TestMethod()]
+        public void GetTest3()
+        {
+            int[] nums = { 1, 1, 3, 3, 3 };
+            string massage = Logic.Mas(nums, 5);
+            Assert.AreEqual("Индексы пары: 0 и 1", massage);
+        }
+        [TestMethod()]
+        public void GetTest4()
+        {
+            int[] nums = { 1, 0, 3, 6, 9 };
+            string massage = Logic.Mas(nums, 5);
+            Assert.AreEqual("Нет такой пары чисел в последовательности", massage);
+        }
+        [TestMethod()]
+        public void GetTest5()
+        {
+            int[] nums = { 1, 8, 0, 2, 9 };
+            string massage = Logic.Mas(nums, 5);
+            Assert.AreEqual("Нет такой пары чисел в последовательности", massage);
+        }
+    }
+}
